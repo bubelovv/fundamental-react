f75c14cd294db89590839d2f7082694f747635dc
Vostokk <sergei.bubelov1@gmail.com>
2022-10-13 15:18:25 +1000

    created new component PostFilter and fixed sorting for post

--- src/App.js
+++ src/App.js
@@ -2,32 +2,30 @@ import React, {useMemo, useState} from "react";
 import './styles/App.css';
 import PostList from "./components/PostList";
 import PostForm from "./UI/PostForm";
-import MySelect from "./UI/MySelect/MySelect";
-import MyInput from "./UI/MyInput/MyInput";
+import PostFilter from "./components/PostFilter";
 
 function App() {
     const [posts, setPosts] = useState([
-        {id: '1', title: 'dd', text: 'bb'},
-        {id: '2', title: 'ee', text: 'cc'},
-        {id: '3', title: 'aa', text: 'dd'},
-        {id: '4', title: 'bb', text: 'ee'},
-        {id: '5', title: 'cc', text: 'aa'},
+        {id: 1, title: 'dd', text: 'bb'},
+        {id: 2, title: 'ee', text: 'cc'},
+        {id: 3, title: 'aa', text: 'dd'},
+        {id: 4, title: 'bb', text: 'ee'},
+        {id: 5, title: 'cc', text: 'aa'},
     ])
 
-    const [selectedSort, setSelectedSort] = useState('')
-    const [searchQuery, setSearchQuery] = useState('')
+    const [filter, setFilter] = useState({sort: '', query: ''})
 
     const sortedPosts = useMemo(() => {
-        console.log('work')
-        if (selectedSort) {
-            return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+        if (filter.sort) {
+            console.log(filter.sort)
+            return [...posts].sort((a, b) => String(a[filter.sort]).localeCompare(String(b[filter.sort])))
         }
         return posts
-    }, [selectedSort, posts])
+    }, [filter.sort, posts])
 
     const searchedSortedPosts = useMemo(() => {
-        return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery.toLowerCase()))
-    }, [searchQuery, sortedPosts])
+        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
+    }, [filter.query, sortedPosts])
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
@@ -37,28 +35,14 @@ function App() {
         setPosts(posts.filter(post => post.id !== id))
     }
 
-    const onChange = (sort) => {
-        setSelectedSort(sort)
-    }
-
     return (
         <div className="App">
             <PostForm createPost={createPost}/>
             <hr style={{margin: '15px 0'}}/>
-            <MyInput
-                placeholder='search post...'
-                value={searchQuery}
-                type='text'
-                onChange={e => setSearchQuery(e.target.value)}/>
-            <MySelect
-                value={selectedSort}
-                onChange={onChange}
-                defaultValue='sort by...'
-                options={[
-                    {value: 'id', name: 'sort by id'},
-                    {value: 'title', name: 'sort by title'},
-                    {value: 'text', name: 'sort by text'},
-                ]}/>
+
+            <PostFilter
+                filter={filter}
+                setFilter={setFilter}/>
 
             {searchedSortedPosts.length
                 ? <PostList remove={removePost} title='My skills' posts={searchedSortedPosts}/>
---
+++ src/components/PostFilter.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import MyInput from "../UI/MyInput/MyInput";
+import MySelect from "../UI/MySelect/MySelect";
+
+const PostFilter = ({filter, setFilter}) => {
+    return (
+        <div>
+            <MyInput
+                placeholder='search post...'
+                value={filter.query}
+                type='text'
+                onChange={e => setFilter({...filter, query: e.target.value})}/>
+            <MySelect
+                value={filter.sort}
+                onChange={sort => {
+                    setFilter({...filter, sort: sort})
+                }}
+                defaultValue='sort by...'
+                options={[
+                    {value: 'id', name: 'sort by id'},
+                    {value: 'title', name: 'sort by title'},
+                    {value: 'text', name: 'sort by text'},
+                ]}/>
+        </div>
+    );
+};
+
+export default PostFilter;
