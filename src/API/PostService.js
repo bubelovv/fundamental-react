0b21a2d9a1984437a2e58c3bc2b2bfea84155329
Vostokk <sergei.bubelov1@gmail.com>
2022-10-25 22:25:27 +1000

    added fetch comments for post by id

--- src/API/PostService.js
+++ src/API/PostService.js
@@ -13,4 +13,8 @@ export default class PostService {
     static async getPostById(id) {
         return await axios.get('https://jsonplaceholder.typicode.com/posts/' + id);
     }
+
+    static async getCommentsById(id) {
+        return await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}/comments`);
+    }
 }
--- src/components/PostPage.jsx
+++ src/components/PostPage.jsx
@@ -6,25 +6,39 @@ import Loader from '../UI/Loader/Loader';
 
 const PostPage = () => {
     const [post, setPost] = useState({});
+    const [comments, setComments] = useState([]);
     const params = useParams();
     const [fetchPostById, isPostLoading, postError] = useFetching(async (id) => {
         const response = await PostService.getPostById(id);
         setPost(response.data);
     });
+    const [fetchCommentsById, isCommentsLoading, commentsError] = useFetching(async (id) => {
+        const response = await PostService.getCommentsById(id);
+        setComments(response.data);
+    });
 
     useEffect(() => {
         fetchPostById(params.id);
+        fetchCommentsById(params.id);
     }, []);
 
     return (
-        isPostLoading
+        isPostLoading || isCommentsLoading
             ? <Loader/>
             : <div style={{textAlign: 'left', margin: 30}}>
                 <h1>This is page of post with ID = {params.id}</h1>
                 <strong>
                     {post.id}. {post.title}
                 </strong>
-                <div>{post.body}</div>
+                <div style={{margin: '0 0 30px'}}>{post.body}</div>
+                <div>
+                    {comments.map(com =>
+                        <div  style={{margin: '0 0 10px'}}>
+                            <strong>{com.id}. {com.email} | </strong><span>{com.name}</span>
+                            <p>{com.body}</p>
+                        </div>
+                    )}
+                </div>
             </div>
     );
 };
